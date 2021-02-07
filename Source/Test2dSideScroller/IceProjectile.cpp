00cead209de1b97491870956b010e77fcad940f1
Yureiii <32612544+yureiii@users.noreply.github.com>
2021-02-07 11:05:52 +0900

    feat: IceProjectile の移動, 時間経過で消滅機能の追加

--- Source/Test2dSideScroller/IceProjectile.cpp
+++ Source/Test2dSideScroller/IceProjectile.cpp
@@ -2,13 +2,24 @@
 
 
 #include "IceProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
 AIceProjectile::AIceProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	// Use a ProjectileMovementComponent to govern this projectile's movement
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement0"));
+	ProjectileMovement->InitialSpeed = 1000.f;
+	ProjectileMovement->MaxSpeed = 1000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->ProjectileGravityScale = 0.f; // No gravity
+
+	// Die after 3 seconds by default
+	InitialLifeSpan = 3.0f;
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +27,5 @@ void AIceProjectile::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-// Called every frame
-void AIceProjectile::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
--- Source/Test2dSideScroller/IceProjectile.h
+++ Source/Test2dSideScroller/IceProjectile.h
@@ -6,21 +6,22 @@
 #include "GameFramework/Actor.h"
 #include "IceProjectile.generated.h"
 
+class UProjectileMovementComponent;
+
 UCLASS()
 class TEST2DSIDESCROLLER_API AIceProjectile : public AActor
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+
 public:	
 	// Sets default values for this actor's properties
 	AIceProjectile();
 
+	FORCEINLINE UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };
